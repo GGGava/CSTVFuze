db8aea185c497bd28e5eb0c50e18e0094896c482
Gustavo Gava <gustavogarciagava@gmail.com>
2025-03-05 16:27:43 -0300

    - Initial commit

---
+++ CSTVFuze/CSTVFuzeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSTVFuzeApp.swift
+//  CSTVFuze
+//
+//  Created by Gustavo Gava on 05/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct CSTVFuzeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
