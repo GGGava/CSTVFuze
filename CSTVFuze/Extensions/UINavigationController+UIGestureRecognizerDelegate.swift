df8c1f2dcf61ee355004f41b3248ec058415e466
Gustavo Gava <gustavogarciagava@gmail.com>
2025-03-08 13:15:29 -0300

    Added dismiss gesture to MatchStatsView - Dismiss gesture was disabled because of the custom back button.

---
+++ CSTVFuze/Extensions/UINavigationController+UIGestureRecognizerDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  UINavigationController+.swift
+//  CSTVFuze
+//
+//  Created by Gustavo Gava on 08/03/2025.
+//
+
+import SwiftUI
+
+extension UINavigationController: @retroactive UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+    
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+    
+}
